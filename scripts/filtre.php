89da9c06759f436c748740054f01424583d421ba
Dromach <dromach30@gmail.com>
2023-11-16 18:09:55 +0100

    yes

--- scripts/filtre.php
+++ scripts/filtre.php
@@ -29,4 +29,54 @@ if ($response !== null) {
 
 <?php
 
+
+// Fonction pour lire les données à partir d'un fichier JSON et filtrer par clé et valeur
+function TrouveData($fichiers, $cle, $valeur)
+{
+    $donneesfiltre = array();
+
+    foreach ($fichiers as $nomfichier) {
+        if (file_exists($nomfichier)) {
+            $json_data = file_get_contents($nomfichier);
+            $data = json_decode('[' . $json_data . ']', true);
+
+            // Filtrer les données en fonction de la clé et de la valeur
+            $resultats = array_filter($data, function ($item) use ($cle, $valeur) {
+                return isset($item[$cle]) && $item[$cle] == $valeur;
+            });
+
+            // Ajouter les résultats au tableau des données filtrées
+            $donneesfiltre[$nomfichier] = $resultats;
+        } else {
+            $donneesfiltre[$nomfichier] = null;
+        }
+    }
+
+    return $donneesfiltre;
+}
+
 ?>
+
+
+<form action="filtre.php" method="post">
+    <label for="fichiers">Sélectionnez les fichiers à comparer :</label>
+    <select name="fichiers[]" id="fichiers" multiple>
+        <option value="naissances-2004-60.json">Oise</option>
+        <option value="naissance-2004-75.json">Paris</option>
+        <option value="naissance-2004-77.json">Seine-et-Marne</option>
+        <option value="naissance-2004-78.json">Yvelines</option>
+        <option value="naissance-2004-91.json">Val-de-Marne</option> 
+        <option value="naissance-2004-92.json">Haut-de-Seine</option>
+        <option value="naissance-2004-93.json">Seine-Saint-Denis</option>
+        <option value="naissance-2004-95.json">Val d'Oise</option>
+        <!-- Ajoutez autant d'options que nécessaire pour vos fichiers -->
+    </select>
+
+    <label for="cle">Clé de filtrage :</label>
+    <input type="text" name="cle" id="cle" required>
+
+    <label for="valeur">Valeur de filtrage :</label>
+    <input type="text" name="valeur" id="valeur" required>
+
+    <button type="submit">Rechercher</button>
+</form>
