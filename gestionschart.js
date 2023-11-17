334717ffcc6750de54ebbf76945c433604b28bab
Dromach <dromach30@gmail.com>
2023-11-17 15:35:57 +0100

    deuxieme modif en esperant qu'elle fonctionne

---
+++ gestionschart.js
@@ -0,0 +1,16 @@
+const FilleGarçon = document.getElementById('Chart');
+      
+        new Chart(FilleGarçon, {
+          type: 'pie',
+          data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1
+            }]
+          },
+          options: {
+
+          }
+        });
