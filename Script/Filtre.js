77bd64105ae788bd3440cd65963a8c24abca3ca9
Dromach <dromach30@gmail.com>
2023-11-19 01:40:18 +0100

    reel test pour le 75

--- Script/Filtre.js
+++ Script/Filtre.js
@@ -1,9 +1,9 @@
-const data = fetch('Données/naissances-2004-60.json');
+const data = fetch('Donnees/naissances-2004-60.json');
 
 const filtre = _.filter(data, {sexe: '1'});
 console.log(filtre);
 
-const data2 = fetch('Données/naissances-2004-75.json');
+const data2 = fetch('Donnees/naissances-2004-75.json');
 
 const filtre2 = _.filter(data2, {sexe: '1'});
 console.log(filtre2);
