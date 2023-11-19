961da67ff4a435dcfb20320f846c7bc05273e262
Dromach <dromach30@gmail.com>
2023-11-19 01:35:00 +0100

    test pour le 75

--- Script/Filtre.js
+++ Script/Filtre.js
@@ -2,3 +2,8 @@ const data = fetch('Données/naissances-2004-60.json');
 
 const filtre = _.filter(data, {sexe: '1'});
 console.log(filtre);
+
+const data2 = fetch('Données/naissances-2004-75.json');
+
+const filtre2 = _.filter(data2, {sexe: '1'});
+console.log(filtre2);
