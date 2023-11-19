744af7e589bf9b1aefae2e76e5cbfa437ce69c7e
DESKTOP-TALTCMI\Celia <celiarebiai70@gmail.com>
2023-11-19 21:01:51 +0100

    graphique prenom

--- Script/recherche.js
+++ Script/recherche.js
@@ -1,50 +1,32 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-var searchInput = document.getElementById('search-input');
-var searchResults = document.getElementById('search-results');
+  var searchInput = document.getElementById('search-input');
+  var searchResults = document.getElementById('search-results');
 
-// Écoutez les événements de saisie dans la barre de recherche
-searchInput.addEventListener('input', function() {
-  var searchTerm = searchInput.value.toLowerCase();
-  displayResults(searchTerm);
-});
+  // Écoutez les événements de saisie dans la barre de recherche
+  searchInput.addEventListener('input', function() {
+    var searchTerm = searchInput.value.toLowerCase();
+    displayResults(searchTerm);
+  });
 
-
-searchResults.addEventListener('click', function(event) {
+  searchResults.addEventListener('click', function(event) {
     // Assurez-vous que l'élément cliqué est un élément de résultat
     if (event.target.classList.contains('result-item')) {
       // Obtenez le texte de l'élément cliqué
       const selectedText = event.target.textContent;
-  
-      // Divisez le texte pour obtenir les valeurs individuelles (prénom, département, année)
-      const [selectedPreusuel, selectedDpt, selectedAnnais] = selectedText.split(' - ');
-  
-      // Chargez les données depuis le fichier JSON
-      fetchData().then(function(data) {
-        // Filtrer les résultats pour le prénom, le département et l'année sélectionnés
-        const selectedData = data.filter(function(item) {
-          return (
-            item.preusuel.toLowerCase() === selectedPreusuel.toLowerCase() &&
-            item.dpt.toLowerCase() === selectedDpt.toLowerCase() &&
-            item.annais.toLowerCase() === selectedAnnais.toLowerCase()
-          );
-        });
-  
-        // Créez un tableau de départements et un tableau de nombres de naissances correspondants
-        const departements = selectedData.map(item => item.dpt);
-        const nombreNaissances = selectedData.map(item => item.nombre);
-  
-        // Appelez une fonction pour créer le graphique à barres
-        createBarChart(departements, nombreNaissances);
-      });
+      
+      // Divisez le texte pour obtenir le prénom
+      const selectedPreusuel = selectedText.trim();
+
+      // Appelez la fonction pour afficher le graphique correspondant au prénom
+      afficherChart(selectedPreusuel);
     }
   });
-  
+
   // Fonction pour créer un graphique à barres
   function createBarChart(departements, nombreNaissances) {
     // Utilisez la bibliothèque de graphiques de votre choix (par exemple, Chart.js)
     // Assurez-vous d'inclure la bibliothèque dans votre projet
-  
+    
     // Exemple avec Chart.js
     var ctx = document.getElementById('barChart').getContext('2d');
     var myChart = new Chart(ctx, {
@@ -67,29 +49,38 @@ searchResults.addEventListener('click', function(event) {
         }
       }
     });
-  } 
-
-// Fonction pour afficher la chart correspondante au prénom
-function afficherChart(preusuel) {
-    // Votre logique pour afficher la chart en fonction du prénom
-    // Par exemple, vous pourriez utiliser une bibliothèque de graphiques comme Chart.js
-    // ou manipuler directement le DOM pour afficher la chart
-    console.log('Afficher la chart pour le prénom :', preusuel);
-}
-
-
-// Fonction pour charger les données depuis le fichier JSON
-async function fetchData() {
-  try {
-    const response = await fetch('Donnees/naissances-2004-60.json');
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erreur lors du chargement des données :', error);
   }
-}
 
-async function displayResults(searchTerm) {
+  // Fonction pour afficher la chart correspondante au prénom
+  function afficherChart(preusuel) {
+    // Chargez les données depuis le fichier JSON
+    fetchData().then(function(data) {
+      // Filtrer les résultats pour le prénom sélectionné
+      const selectedData = data.filter(function(item) {
+        return item.preusuel.toLowerCase() === preusuel.toLowerCase();
+      });
+
+      // Créez un tableau de départements et un tableau de nombres de naissances correspondants
+      const departements = selectedData.map(item => item.dpt);
+      const nombreNaissances = selectedData.map(item => item.nombre);
+
+      // Appelez une fonction pour créer le graphique à barres
+      createBarChart(departements, nombreNaissances);
+    });
+  }
+
+  // Fonction pour charger les données depuis le fichier JSON
+  async function fetchData() {
+    try {
+      const response = await fetch('Donnees/naissances-2004-93.json');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Erreur lors du chargement des données :', error);
+    }
+  }
+
+  async function displayResults(searchTerm) {
     // Réinitialiser la liste des résultats à chaque saisie
     searchResults.innerHTML = '';
   
@@ -118,4 +109,6 @@ async function displayResults(searchTerm) {
     } else {
       searchResults.style.display = 'none';
     }
-  }})
+  }
+});
+  
