747610d727d19416b7a0c7b96240ef97c1f2f0c9
Dromach <dromach30@gmail.com>
2023-11-19 08:39:27 +0100

    filtre.js fonctionnel

--- Script/Filtre.js
+++ Script/Filtre.js
@@ -1,9 +1,31 @@
-const data = fetch('Donnees/naissances-2004-60.json');
+document.getElementById('informations').addEventListener('change', function() {
+    const selectedFiles = Array.from(this.selectedOptions).map(option => option.value);
+    selectedFiles.forEach(file => {
+        sexeGraph(file);
+    });
+});
 
-const filtre = _.filter(data, {sexe: '1'});
-console.log(filtre);
 
-const data2 = fetch('Donnees/naissances-2004-75.json');
-
-const filtre2 = _.filter(data2, {sexe: '1'});
-console.log(filtre2);
+var myPieChart = null;
+var config = {options:{},type:'pie'}
+async function sexeGraph(donnee) {
+    const test = await fetch(donnee);
+    const data = await test.json();
+    const filtreH = _.filter(data, { sexe: 1 });
+    const filtreF = _.filter(data, { sexe: 2 });
+ var totalH = _.sumBy(filtreH, 'nombre'),
+    totalF = _.sumBy(filtreF, 'nombre'),
+    ctx = document.getElementById('sexe').getContext('2d');
+                config.data = {
+    labels: ["Hommes","Femmes"],
+    datasets:
+       [{
+          data: [totalH,totalF]
+       }]
+ };
+ if(myPieChart == null){
+                    myPieChart = new Chart(ctx, config);
+                }else{
+                    myPieChart.update()
+                }
+}
