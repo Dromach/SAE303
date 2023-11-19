dc0d10de967b0f67f53b556b18cfe2e6f7c37963
DESKTOP-TALTCMI\Celia <celiarebiai70@gmail.com>
2023-11-19 20:23:19 +0100

    Barre de recherche

---
+++ Script/recherche.js
@@ -0,0 +1,50 @@
+var searchInput = document.getElementById('search-input');
+var searchResults = document.getElementById('search-results');
+
+// Écoutez les événements de saisie dans la barre de recherche
+searchInput.addEventListener('input', function() {
+  var searchTerm = searchInput.value.toLowerCase();
+  displayResults(searchTerm);
+});
+
+// Fonction pour charger les données depuis le fichier JSON
+async function fetchData() {
+    try {
+      const response = await fetch('naissances-2004-60.json');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Erreur lors du chargement des données :', error);
+    }
+  }
+  
+  async function displayResults(searchTerm) {
+      // Réinitialiser la liste des résultats à chaque saisie
+      searchResults.innerHTML = '';
+    
+      // Charger les données depuis le fichier JSON
+      const data = await fetchData();
+    
+      // Filtrer les résultats en fonction du terme de recherche
+      var filteredResults = data.filter(function(item) {
+        return (
+          item.preusuel.toLowerCase().includes(searchTerm)
+          // Ajoutez d'autres propriétés selon vos besoins
+        );
+      });
+    
+      // Afficher les résultats filtrés
+      filteredResults.forEach(function(item) {
+        var resultItem = document.createElement('li');
+        resultItem.className = 'result-item';
+        resultItem.textContent = `${item.preusuel} `;
+        searchResults.appendChild(resultItem);
+      });
+    
+      // Afficher la liste des résultats
+      if (filteredResults.length > 0) {
+        searchResults.style.display = 'block';
+      } else {
+        searchResults.style.display = 'none';
+      }
+    }
