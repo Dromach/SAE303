6c545e797c517df9c2ab0a1d98013fcb55a592a0
Dromach <dromach30@gmail.com>
2023-11-19 01:18:30 +0100

    remise a la normal

--- Script/Filtre.js
+++ Script/Filtre.js
@@ -1,14 +1,4 @@
-//1 = boug 2 = meuf
-
-/*const data = fetch('Données/naissances-2004-60.json');
+const data = fetch('Données/naissances-2004-60.json');
 
 const filtre = _.filter(data, {"sexe" : 1});
-console.log(filtre);*/
-
-const data = fetch('Données/test.json');
-
-const filtre = _.filter(data, {name: 'John'});
 console.log(filtre);
-
-
-//console.log(filter(test, {"sexe" : 1}));
